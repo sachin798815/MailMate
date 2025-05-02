23154de638f9960bcbf94f3c641279bc7c674855
sachin798815 <sachin798815@gmail.com>
2025-05-02 19:54:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// src/App.jsx
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+      <h1 className="text-4xl font-bold text-blue-400">Welcome to MailMate</h1>
+    </div>
+  );
+}
+
