ada4a038cce10e6efd3fcfebdd8d1a9765bb0628
sachin798815 <sachin798815@gmail.com>
2025-05-03 23:44:12 +0530

    browser router moved to main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+// App.jsx
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import TopNav from './components/TopNav';
 import SideNav from './components/SideNav';
@@ -8,23 +9,21 @@ import SideNav from './components/SideNav';
 
 const App = () => {
   return (
-    <BrowserRouter> {/* Wrap everything in BrowserRouter */}
-      <div className="flex">
-        {/* SideNav */}
-        <SideNav />
+    <div className="flex">
+      {/* SideNav */}
+      <SideNav />
 
-        <div className="flex-1">
-          {/* TopNav */}
-          <TopNav />
+      <div className="flex-1">
+        {/* TopNav */}
+        <TopNav />
 
-          {/* Routes */}
-          <Routes>
-            {/* <Route path="/" element={<HomePage />} />
-            <Route path="/inbox" element={<InboxPage />} /> */}
-          </Routes>
-        </div>
+        {/* Routes */}
+        <Routes>
+          {/* <Route path="/" element={<HomePage />} />
+          <Route path="/inbox" element={<InboxPage />} /> */}
+        </Routes>
       </div>
-    </BrowserRouter>
+    </div>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import App from './App'; // Import App component
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App /> {/* No BrowserRouter here anymore */}
-  </StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <BrowserRouter>
+    <App /> {/* Wrap App with BrowserRouter */}
+  </BrowserRouter>
 );
